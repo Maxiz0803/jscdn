bc78489b458fa25397594174c0a8eb4c8620a3f1
mxk-zwh <72368111+mxk-zwh@users.noreply.github.com>
2023-08-26 20:22:45 +0800

    Update 1-0_电池监控.user.js

--- monkey/1-0_电池监控.user.js
+++ monkey/1-0_电池监控.user.js
@@ -33,15 +33,10 @@ group1:
 
 (function () {
     'use strict';
-    // 低电量
     var lowStandard = GM_getValue("group1.configA");
     const percentIcon = '<svg  class="uuuu" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="33" height="29" viewBox="0 0 33 29" fill="none"><g opacity="1"  transform="translate(0 0)  rotate(0)"><path id="圆形 1" fill-rule="evenodd" style="fill:#FFFFFF" opacity="1" d="M7,3.77c-1.62,0 -2.94,1.22 -2.94,2.73c0,1.51 1.32,2.73 2.94,2.73c1.62,0 2.94,-1.22 2.94,-2.73c0,-1.51 -1.32,-2.73 -2.94,-2.73zM14,6.5c0,3.59 -3.13,6.5 -7,6.5c-3.87,0 -7,-2.91 -7,-6.5c0,-3.59 3.13,-6.5 7,-6.5c3.87,0 7,2.91 7,6.5z"></path><path id="圆形 1" fill-rule="evenodd" style="fill:#FFFFFF" opacity="1" d="M26,19.77c-1.62,0 -2.94,1.22 -2.94,2.73c0,1.51 1.32,2.73 2.94,2.73c1.62,0 2.94,-1.22 2.94,-2.73c0,-1.51 -1.32,-2.73 -2.94,-2.73zM33,22.5c0,3.59 -3.13,6.5 -7,6.5c-3.87,0 -7,-2.91 -7,-6.5c0,-3.59 3.13,-6.5 7,-6.5c3.87,0 7,2.91 7,6.5z"></path><path id="路径 2" fill-rule="evenodd" style="fill:#FFFFFF" opacity="1" d="M9 27.5L28.5 1L24 1L4.5 27.5L9 27.5Z"></path><path  id="路径 2" style="fill:#FFFFFF; opacity:1;" d="M8.59728,27.2037l19.50002,-26.50004l0.4027,0.29634v0.5h-4.5v-0.5l0.4027,0.29634l-19.49998,26.49996l-0.40272,-0.2963v-0.5h4.5v0.5zM9.25286,28h-5.74156l20.2358,-27.5h5.7416l-20.08598,27.2963z"></path></g></svg>'
-    // 闪电图标
-    const ligntningIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="vvvv" xmlns:xlink="http://www.w3.org/1999/xlink" width="28" height="50" viewBox="0 0 28 50" fill="none"><path id="路径 1" fill-rule="evenodd" style="fill:#FFFFFF" opacity="1" d="M4.25 21.7106L11.3 21.7106C12.16 21.7006 12.63 22.7206 12.06 23.3706L1.44 35.5506C0.87 36.2006 1.34 37.2206 2.2 37.2106L10.13 37.2106C10.8 37.2106 11.28 37.8706 11.08 38.5106L7.5 49.7106L25.44 30.3906C26.03 29.7506 25.58 28.7106 24.71 28.7106L17.8 28.7106C16.93 28.7106 16.48 27.6706 17.07 27.0306L27.93 15.3906C28.52 14.7506 28.07 13.7106 27.2 13.7106L18.73 13.7106C18.1 13.7106 17.62 13.1306 17.75 12.5106L20 1.71063L3.5 20.0406C2.92 20.6906 3.38 21.7206 4.25 21.7106Z"></path></svg>'
-    // 网页内 消息提示 new一个
+    const lightningIcon = '<svg class="vvvv" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28" height="50" viewBox="0 0 28 50" fill="none"><path id="路径 1" fill-rule="evenodd" style="fill:#FFFFFF" opacity="1" d="M4.25 21.7106L11.3 21.7106C12.16 21.7006 12.63 22.7206 12.06 23.3706L1.44 35.5506C0.87 36.2006 1.34 37.2206 2.2 37.2106L10.13 37.2106C10.8 37.2106 11.28 37.8706 11.08 38.5106L7.5 49.7106L25.44 30.3906C26.03 29.7506 25.58 28.7106 24.71 28.7106L17.8 28.7106C16.93 28.7106 16.48 27.6706 17.07 27.0306L27.93 15.3906C28.52 14.7506 28.07 13.7106 27.2 13.7106L18.73 13.7106C18.1 13.7106 17.62 13.1306 17.75 12.5106L20 1.71063L3.5 20.0406C2.92 20.6906 3.38 21.7206 4.25 21.7106Z"></path></svg>'
     const message = new Message();
-    // 日期 缺零 补全
-    console.log(lowStandard)
     function dateFilter(date) {
         return date < 10 ? "0" + date : date;
     }
@@ -99,126 +94,126 @@ group1:
         }
     }
     GM_addStyle(`
-             .batteryShape{
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    opacity: 0;
-                    position: fixed;
-                    z-index: 10010;
-                    width: 100%;
-                    height: 36px;
-                    margin: 0px;
-                    color: #fff;
-                    background-color: rgb(43 43 43 / 25%);
-                    box-sizing: content-box;
-                    border-bottom: 1px solid #000dff;
-                    backdrop-filter: blur(10px);
-                    justify-content: space-between;
-                    transition: all .3s ease;
-                }
-                .batteryShape:hover{
-                    opacity: 1;
-                }
-                .bflex{
-                    display: flex;
-                    align-items: center;
-                }
-                .batteryShape .left{
-                    padding: 2px;
-                    border-radius: 5px;
-                    margin-left: 4px;
-                    padding-top: 3px;
-                    padding-right: 8px;
-                }
-                .batteryShape .left:hover{
-                    background: #9366e340;
-                }
-                .batteryShape .left .ampm {
-                    margin-left: 5px;
-                    font-weight: bold;
-                    letter-spacing: 1px;
-                }
-                .batteryShape .left #current_time{
-                    font-weight: bold;
-                }
-                .batteryShape .center{
-                 margin: 0 auto;
-                 text-align:center;
-                }
-                .batteryShape .right{
-                    padding: 2px;
-                    border-radius: 5px;
-                    margin-right: 4px;
-                    margin-left: auto;
-                }
-                .batteryShape .right:hover{
-                    background: #9366e340;
-                }
-                .batteryShape .right .battery{
-                     margin-right: 5px;
-                }
-                .battery_level{
-                    display:flex;
-                }
-                .batteryShape .right .battery .dianliang{
-                    font-weight: bold;
-                    font-size: 15px;
-                    text-align: center;
-                }
-                .batteryShape .right .battery .percent_icon{
-                    font-size: 8px;
-                }
-                .batteryShape .right .bbb{
-                    margin-left: 5px;
-                    margin-right: 5px;
-                    width: 28px;
-                    height: 10px;
-                    border-radius: 10px!important;
-                    border:2px solid #ed333300;
-                    outline: 3px solid #fff;
-                    box-sizing: content-box;
-                    overflow: hidden;
-                }
-                .batteryShape .right .set_battery_state{
-                    height: 15px;
-                    background: white;
-                    background-size: 38px 26px;
-                    box-sizing: content-box;
-                    position: relative;
-                    z-index: 0;
-                    width: 100%;
-                }
-                .batteryShape .right .batteryTime{
-                    font-size:20px;
-                    display: flex;
-                    align-items: center;
-                }
-                svg.vvvv {
-                    width: 0.6em;
-                    height: 0.7em;
-                    fill: currentcolor;
-                    overflow: hidden;
-                    animation: infinite 3s blink;
-                }
-                svg.uuuu {
-                    width: 1em;
-                    height: 1em;
-                    fill: currentcolor;
-                }
-                @keyframes blink {
-                    0%{opacity: 0;}
-                    60%{opacity: 1;}
-                    100%{opacity: 0;}
-                }
-                @media screen and (width: 600px) {
-                    body {
-                        color: red;
-                    }
-                }
+        .batteryShape{
+            top: 0;
+            left: 0;
+            right: 0;
+            opacity: 0;
+            position: fixed;
+            z-index: 10010;
+            width: 100%;
+            height: 36px;
+            margin: 0px;
+            color: #fff;
+            background-color: rgb(43 43 43 / 25%);
+            box-sizing: content-box;
+            border-bottom: 1px solid #000dff;
+            backdrop-filter: blur(10px);
+            justify-content: space-between;
+            transition: all .3s ease;
+        }
+        .batteryShape:hover{
+            opacity: 1;
+        }
+        .bflex{
+            display: flex;
+            align-items: center;
+        }
+        .batteryShape .left{
+            padding: 2px;
+            border-radius: 5px;
+            margin-left: 4px;
+            padding-top: 3px;
+            padding-right: 8px;
+        }
+        .batteryShape .left:hover{
+            background: #9366e340;
+        }
+        .batteryShape .left .ampm {
+            margin-left: 5px;
+            font-weight: bold;
+            letter-spacing: 1px;
+        }
+        .batteryShape .left #current_time{
+            font-weight: bold;
+        }
+        .batteryShape .center{
+            margin: 0 auto;
+            text-align:center;
+        }
+        .batteryShape .right{
+            padding: 2px;
+            border-radius: 5px;
+            margin-right: 4px;
+            margin-left: auto;
+        }
+        .batteryShape .right:hover{
+            background: #9366e340;
+        }
+        .batteryShape .right .battery{
+                margin-right: 5px;
+        }
+        .battery_level{
+            display:flex;
+        }
+        .batteryShape .right .battery .dianliang{
+            font-weight: bold;
+            font-size: 15px;
+            text-align: center;
+        }
+        .batteryShape .right .battery .percent_icon{
+            font-size: 8px;
+        }
+        .batteryShape .right .bbb{
+            margin-left: 5px;
+            margin-right: 5px;
+            width: 28px;
+            height: 10px;
+            border-radius: 10px!important;
+            border:2px solid #ed333300;
+            outline: 3px solid #fff;
+            box-sizing: content-box;
+            overflow: hidden;
+        }
+        .batteryShape .right .set_battery_state{
+            height: 15px;
+            background: white;
+            background-size: 38px 26px;
+            box-sizing: content-box;
+            position: relative;
+            z-index: 0;
+            width: 100%;
+        }
+        .batteryShape .right .batteryTime{
+            font-size:20px;
+            display: flex;
+            align-items: center;
+        }
+        svg.vvvv {
+            width: 0.6em;
+            height: 0.7em;
+            fill: currentcolor;
+            overflow: hidden;
+            animation: infinite 3s blink;
+        }
+        svg.uuuu {
+            width: 1em;
+            height: 1em;
+            fill: currentcolor;
+        }
+        @keyframes blink {
+            0%{opacity: 0;}
+            60%{opacity: 1;}
+            100%{opacity: 0;}
+        }
+        @media screen and (width: 600px) {
+            body {
+                color: red;
+            }
+        }
         `);
     GM_addStyle(GM_getResourceText("css"));
-    var html = `
+    $('body').append(`
         <link rel="stylesheet" href="//at.alicdn.com/t/font_1117508_wxidm5ry7od.css">
         <bbb class="batteryShape bflex">
             <bbb class="left bflex">
@@ -233,7 +228,7 @@ group1:
 
                     </bbb>
                     <bbb class="batteryTime">
-                    ${ligntningIcon}
+                    ${lightningIcon}
                     </bbb>
                     <bbb class="battery_level">
                         <bbb class="dianliang">99</bbb>
@@ -242,8 +237,7 @@ group1:
                 </bbb>
             </bbb>
         </bbb>
-        `;
-    $('body').append(html);
+        `);
     function leftClock() {
         const today = new Date();
         const hour = today.getHours();
